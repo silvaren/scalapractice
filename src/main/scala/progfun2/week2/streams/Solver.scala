744da4cb143364ca6c5f0c21dd5e169fd32c8930
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-10-19 23:52:38 -0200

    Add quickcheck code sample

--- src/main/scala/progfun2/week2/streams/Solver.scala
+++ src/main/scala/progfun2/week2/streams/Solver.scala
@@ -28,7 +28,7 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = b.legalNeighbors.map((b))
 
   /**
    * This function returns the list of neighbors without the block
