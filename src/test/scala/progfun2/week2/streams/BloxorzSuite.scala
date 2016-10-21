67ea65c0475f719b397f12b4b2d827627e1e1b2c
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-10-21 01:02:13 -0200

    Add neighborswithhistory test

--- src/test/scala/progfun2/week2/streams/BloxorzSuite.scala
+++ src/test/scala/progfun2/week2/streams/BloxorzSuite.scala
@@ -62,6 +62,16 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("neighbors with history") {
+    new Level1 {
+      val expected = Set(
+        (Block(Pos(1,2),Pos(1,3)), List(Right,Left,Up)),
+        (Block(Pos(2,1),Pos(3,1)), List(Down,Left,Up))
+      )
+      val actual = neighborsWithHistory(Block(Pos(1,1),Pos(1,1)), List(Left,Up))
+      assert(actual.toSet == expected)
+    }
+  }
 
 	test("optimal solution for level 1") {
     new Level1 {
