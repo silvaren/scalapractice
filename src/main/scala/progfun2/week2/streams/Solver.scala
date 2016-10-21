b7ed760a411951be05f61d0b05cd826eab67d008
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-10-20 23:43:44 -0200

    Fix neighbors with history

--- src/main/scala/progfun2/week2/streams/Solver.scala
+++ src/main/scala/progfun2/week2/streams/Solver.scala
@@ -28,7 +28,8 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = b.legalNeighbors.map((b))
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] =
+    b.legalNeighbors.toStream.map{case (block, move) => (block, move :: history)}
 
   /**
    * This function returns the list of neighbors without the block
