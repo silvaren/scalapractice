a357deb62d4a8ae9559e2b622716d87a083c40ae
Renato Oliveira <oliveirasilvaren@gmail.com>
2016-10-31 22:28:16 -0200

    Fix solution signal construction

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -8,12 +8,13 @@ object Polynomial {
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
                        c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    if (delta() < 0) Signal{Set()}
-    else Signal{
-      Set(
-        ( -b() + Math.sqrt(delta()) ) / ( 2.0 * a() ),
-        ( -b() - Math.sqrt(delta()) ) / ( 2.0 * a() )
-      )
+    Signal{
+      if (delta() < 0) Set()
+      else
+        Set(
+          ( -b() + Math.sqrt(delta()) ) / ( 2.0 * a() ),
+          ( -b() - Math.sqrt(delta()) ) / ( 2.0 * a() )
+        )
     }
   }
 }
